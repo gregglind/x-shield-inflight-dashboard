d41efec457c0cebd6c7c8dd0c2834eb532519508
Gregg Lind <gregg.lind@gmail.com>
2017-08-03 10:47:15 -0500

    react-table integration is GOOD ENOUGH, see https://www.npmjs.com/package/react-table

--- inflight-dash-prototype-1/src/App.js
+++ inflight-dash-prototype-1/src/App.js
@@ -1,6 +1,47 @@
 import React, { Component } from 'react';
+import { Grid, Navbar, Jumbotron, Button } from 'react-bootstrap';
+import ReactTable from 'react-table';
+import 'react-table/react-table.css'
+
 import logo from './logo.svg';
 import './App.css';
+
+class ExampleTable extends Component {
+  render() {
+    const data = [{
+      name: 'Tanner Linsley',
+      age: 26,
+      friend: {
+        name: 'Jason Maurer',
+        age: 23,
+      }
+    }];
+
+    const columns = [{
+      Header: 'Name',
+      accessor: 'name' // String-based value accessors!
+    }, {
+      Header: 'Age',
+      accessor: 'age',
+      Cell: props => <span className='number'>{props.value}</span> // Custom cell components!
+      }, {
+        id: 'friendName', // Required because our accessor is not a string
+        Header: 'Friend Name',
+        accessor: d => d.friend.name // Custom value accessors!
+      }, {
+        Header: props => <span>Friend Age</span>, // Custom header components!
+        accessor: 'friend.age'
+    }
+    ];
+
+    return (
+      <ReactTable
+        data={data}
+        columns={columns}
+      />
+    )
+  }
+}
 
 class App extends Component {
   render() {
@@ -13,7 +54,70 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+      <ExampleTable />
+
+      {/* example */}
+      <div>
+        <Navbar inverse fixedTop>
+          <Grid>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <a href="/">React App</a>
+              </Navbar.Brand>
+              <Navbar.Toggle />
+            </Navbar.Header>
+          </Grid>
+        </Navbar>
+        <Jumbotron>
+          <Grid>
+            <h1>Welcome to React</h1>
+            <p>
+              <Button
+                bsStyle="success"
+                bsSize="large"
+                href="http://react-bootstrap.github.io/components.html"
+                target="_blank">
+                View React Bootstrap Docs
+              </Button>
+            </p>
+          </Grid>
+        </Jumbotron>
       </div>
+
+      {/* comment */}
+      <div className="container">
+        <h2>Basic Table</h2>
+        <p>The .table class adds basic styling (light padding and only horizontal dividers) to a table:</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Firstname</th>
+              <th>Lastname</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>John</td>
+              <td>Doe</td>
+              <td>john@example.com</td>
+            </tr>
+            <tr>
+              <td>Mary</td>
+              <td>Moe</td>
+              <td>mary@example.com</td>
+            </tr>
+            <tr>
+              <td>July</td>
+              <td>Dooley</td>
+              <td>july@example.com</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+    </div>
     );
   }
 }
